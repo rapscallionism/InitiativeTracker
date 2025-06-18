ab286eac3f5771ca2c44c36576caaaac6781c2f8
Caleb Ninal <marvin.ninal365@gmail.com>
2025-06-17 21:23:29 -0500

    feat: making render of the components

---
+++ Classes/CharacterClass.cs
@@ -0,0 +1,27 @@
+﻿namespace InitiativeTracker.Classes
+{
+    public class CharacterClass
+    {
+        public int id;
+        public string name;
+        public string characterImageUrl = "";
+        public Armor? helm;
+        public Clothing? cape;
+        public Armor? chest;
+        public Armor? gloves;
+        public Armor? boots;
+        public Ring? ringOne;
+        public Ring? ringTwo;
+        public Necklace? necklace;
+        public Melee? mainMelee;
+        public Melee? offMelee;
+        public Ranged? mainRanged;
+        public Ranged? offRanged;
+
+        public CharacterClass()
+        {
+
+        }
+
+    }
+}
