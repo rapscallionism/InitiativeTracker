805a61a7a74f8c2facc2c433321381ec9b7bc876
Caleb Ninal <marvin.ninal365@gmail.com>
2025-06-17 20:50:01 -0500

    feat: adding character details; will need a database

---
+++ Classes/Character.cs
@@ -0,0 +1,20 @@
+﻿namespace InitiativeTracker.Classes
+{
+    public class Character
+    {
+        int id;
+        string name;
+        Armor? helm;
+        Clothing? cape;
+        Armor? chest;
+        Armor? gloves;
+        Armor? boots;
+        Ring? ringOne;
+        Ring? ringTwo;
+        Necklace? necklace;
+        Melee? mainMelee;
+        Melee? offMelee;
+        Ranged? mainRanged;
+        Ranged? offRanged;
+    }
+}
