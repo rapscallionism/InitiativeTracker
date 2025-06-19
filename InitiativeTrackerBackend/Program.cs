c2461eeb323ffc1f18ddd21ed68d22364b5061dc
Caleb Ninal <marvin.ninal365@gmail.com>
2025-06-19 00:48:45 -0500

    feat: updating to use nosql

--- InitiativeTrackerBackend/Program.cs
+++ InitiativeTrackerBackend/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,20 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Bind settings
+builder.Services.Configure<MongoDbSettings>(builder.Configuration.GetSection("MongoDbSettings"));
+
+builder.Services.AddSingleton<IMongoClient>(sp =>
+{
+    var settings = sp.GetRequiredService<IOptions<MongoDbSettings>>().Value;
+
+    var credential = MongoCredential.CreateCredential(settings.Database, settings.Username, settings.Password);
+    var mongoSettings = MongoClientSettings.FromUrl(new MongoUrl($"mongodb://{settings.Host}:{settings.Port}"));
+    mongoSettings.Credential = credential;
+
+    return new MongoClient(mongoSettings);
+});
 
 var app = builder.Build();
 
