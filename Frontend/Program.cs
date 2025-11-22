9b2c53f0a9e2526d163fb30047685926c6c3459d
Caleb Ninal <marvin.ninal365@gmail.com>
2025-11-21 23:50:19 -0600

    feat: removing mudblazor as a dep, just sticking with bootstrap i guess

--- Frontend/Program.cs
+++ Frontend/Program.cs
@@ -1,14 +1,14 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Frontend;
-using MudBlazor.Services;
+//using MudBlazor.Services;
 using Frontend.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddMudServices();
+//builder.Services.AddMudServices();
 
 builder.Services.AddScoped<MonsterLoaderService>();
 
