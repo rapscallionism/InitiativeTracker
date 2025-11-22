a87974a33aee2e1c4dca953f046089762705d5c2
Caleb Ninal <marvin.ninal365@gmail.com>
2025-11-22 01:44:48 -0600

    feat: adding in models to represent the table that will eventually becreated

---
+++ Backend/Models/Entities/ActionEntity.cs
@@ -0,0 +1,6 @@
+﻿namespace Backend.Models.Entities
+{
+    public class ActionEntity : BaseActionEntity
+    {
+    }
+}
---
+++ Backend/Models/Entities/ReactionEntity.cs
@@ -0,0 +1,6 @@
+﻿namespace Backend.Models.Entities
+{
+    public class ReactionEntity : BaseActionEntity
+    {
+    }
+}
