6991411ad80d3c793853139a2e116b8e861f724e
Caleb Ninal <marvin.ninal365@gmail.com>
2025-11-22 01:44:48 -0600

    feat: adding in models to represent the table that will eventually becreated

---
+++ Backend/Models/Entities/BaseActionEntity.cs
@@ -0,0 +1,42 @@
+﻿namespace Backend.Models.Entities
+{
+    /// <summary>
+    ///     Represents the basic structure of the entity for an action. In this case,
+    ///     the "action" refers to the action economy type (action, bonus action, reaction, etc.).
+    /// </summary>
+    public class BaseActionEntity
+    {
+        /// <summary>
+        ///     Unique identifier.
+        /// </summary>
+        public string Id { get; set; }
+
+        /// <summary>
+        ///     The unique name of this specific action economy.
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        ///     A brief description of what this specific action
+        ///     will occur.
+        /// </summary>
+        public string Description { get; set; }
+        
+        /// <summary>
+        ///     A numerical value indicating how much charge this will
+        ///     cost the item if this action is utilized. By default,
+        ///     this should be 0 or null to represent that it does
+        ///     not cost any charge since, typically, there are some
+        ///     which do not require a charge to occur (e.g.
+        ///     enchantments, passive effects, etc.)
+        /// </summary>
+        public int? ChargeCost { get; set; }
+
+        /// <summary>
+        ///     Unique id in which this action economy type 
+        ///     ties back to the equipment this entity belongs
+        ///     to.
+        /// </summary>
+        public string EquipmentId { get; set; }
+    }
+}
---
+++ Backend/Models/Entities/EffectEntity.cs
@@ -0,0 +1,9 @@
+﻿namespace Backend.Models.Entities
+{
+    /// <summary>
+    ///     Typically, this will be passive and have no charge cost.
+    /// </summary>
+    public class EffectEntity : BaseActionEntity
+    {
+    }
+}
