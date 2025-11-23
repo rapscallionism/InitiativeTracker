6e7b79c940ef042b771332c7928053e6e282f891
Caleb Ninal <marvin.ninal365@gmail.com>
2025-11-22 23:57:48 -0600

    feat: added in missing change

---
+++ Core/Models/Utilities/ActionEconomyType.cs
@@ -0,0 +1,25 @@
+﻿namespace Core.Models.Utilities
+{
+    public enum ActionEconomyType
+    {
+        /// <summary>
+        ///     Costs an Action to activate.
+        /// </summary>
+        ACTION,
+
+        /// <summary>
+        ///     Costs a Bonus Action to activate.
+        /// </summary>
+        BONUS_ACTION,
+
+        /// <summary>
+        ///     Costs a Reaction to activate.
+        /// </summary>
+        REACTION,
+
+        /// <summary>
+        ///     Costs nothing to activate, is permanently on.
+        /// </summary>
+        EFFECT,
+    }
+}
