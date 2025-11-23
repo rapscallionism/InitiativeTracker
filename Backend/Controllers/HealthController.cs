e54d02fe1b0477d9fae3ca24ce58f769b57b33c1
Caleb Ninal <marvin.ninal365@gmail.com>
2025-11-23 00:38:04 -0600

    feat: made it so that now it runs the frontend and backend

---
+++ Backend/Controllers/HealthController.cs
@@ -0,0 +1,36 @@
+﻿using Core.Interfaces;
+using Core.Models.DTOs;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Backend.Controllers
+{
+    [ApiController]
+    [Route("api/health")]
+    public class HealthController : ControllerBase
+    {
+        private readonly ILogger<HealthController> _logger;
+        private readonly IEquipmentService _service;
+
+        public HealthController(ILogger<HealthController> logger, IEquipmentService service)
+        {
+            _logger = logger;
+            _service = service;
+        }
+
+        [HttpGet("ping")]
+        public async Task<ActionResult<List<EquipmentDTO>>> Ping()
+        {
+            try
+            {
+                var allEquipment = await _service.GetAllEquipment();
+                return Ok(allEquipment);
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e.Message);
+                _logger.LogError(e.StackTrace);
+                return StatusCode(500, "Something went wrong...");
+            }
+        }
+    }
+}
