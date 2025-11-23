03fe5af256586e94c8c6959183e620a9819f279c
Caleb Ninal <marvin.ninal365@gmail.com>
2025-11-23 00:12:04 -0600

    feat: implementing basic service logic; adding in missing field few value

--- Core/Models/Utilities/ValidateModel.cs
+++ Core/Models/Utilities/ValidateModel.cs
@@ -6,5 +6,19 @@
         public List<string> FailedValidations { get; set; } = [];
         public DTO? Source { get; set; }
         public Entity? Destination { get; set; }
+        
+        public ValidateModel(DTO source, Entity destination)
+        {
+            this.Source = Source;
+            this.Destination = Destination;
+        }
+
+        public ValidateModel(bool isValid, List<string> failedValidationMsgs, DTO source, Entity destination)
+        {
+            this.IsValid = isValid;
+            this.FailedValidations = failedValidationMsgs;
+            this.Source = Source;
+            this.Destination = Destination;
+        }
     }
 }
